3bb7a66d7b6cd4e0d97873473319731c688b07c4
Jonthan <jrespeto@users.noreply.github.com>
2019-09-26 11:36:43 -0400

    Added GRE

--- specs.py
+++ specs.py
@@ -527,6 +527,15 @@ s7_data="Return Code:8,Transport Size:8,Data Length:16"
 dns="Identification:16,Qr:1,Opcode:4,DNS Flags:7,Rcode:4,Totle Questions:16, Total Answers:16,Total Auth Resource Recs:16,Total Additional Res. Recs:16"
 
 
+# 0                   1                   2                   3
+# 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|C|       Reserved0       | Ver |         Protocal Type         |
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|      Checksum (optional)      |           Reserved1           |
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+gre="C:1,Reserved0:12,Ver:3,Protocal Type:16,Checksum (optional):16,Reserved1:16"
+
 #      0                   1
 #      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
 #     *-------------------------------*
