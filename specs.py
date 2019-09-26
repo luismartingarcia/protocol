b88f6fb6e09e9e7dc91802b1075c63fbec66f4ca
Jonthan <jrespeto@users.noreply.github.com>
2019-09-26 11:55:21 -0400

    Fix typo

--- specs.py
+++ specs.py
@@ -530,11 +530,11 @@ dns="Identification:16,Qr:1,Opcode:4,DNS Flags:7,Rcode:4,Totle Questions:16, Tot
 # 0                   1                   2                   3
 # 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|C|       Reserved0       | Ver |         Protocal Type         |
+#|C|       Reserved0       | Ver |         Protocol Type         |
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|      Checksum (optional)      |           Reserved1           |
+#|      Checksum (optional)      |      Reserved1 (Optional)     |
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-gre="C:1,Reserved0:12,Ver:3,Protocal Type:16,Checksum (optional):16,Reserved1:16"
+gre="C:1,Reserved0:12,Ver:3,Protocol Type:16,Checksum (optional):16,Reserved1 (Optional):16"
 
 #      0                   1
 #      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
